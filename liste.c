073732e36144142fd90581401b8fee170cce70f0
David Vivier <contact@davidvivier.fr>
2017-02-13 15:10:28 +0100

    Declare player structure, init players tab

--- liste.c
+++ liste.c
@@ -3,3 +3,28 @@
 //
 
 #include "liste.h"
+
+
+void init_joueurs(Joueur joueurs[]) {
+
+	strcpy(joueurs[0].nom, "X");
+	joueurs[0].precedent = 4;
+	joueurs[0].suivant = 2;
+
+	strcpy(joueurs[0].nom, "A");
+	joueurs[0].precedent = 2;
+	joueurs[0].suivant = 3;
+	
+	strcpy(joueurs[0].nom, "M");
+	joueurs[0].precedent = 0;
+	joueurs[0].suivant = 1;
+	
+	strcpy(joueurs[0].nom, "R");
+	joueurs[0].precedent = 1;
+	joueurs[0].suivant = 4;
+
+	strcpy(joueurs[0].nom, "C");
+	joueurs[0].precedent = 3;
+	joueurs[0].suivant = 0;
+
+}
--- liste.h
+++ liste.h
@@ -5,4 +5,20 @@
 #ifndef TP_LISTE1_LISTE_H
 #define TP_LISTE1_LISTE_H
 
+#include <string.h>
+
+#define NB_JOUEURS 10
+
+
+typedef struct Joueur {
+	char nom[20];
+	int precedent;
+	int suivant;
+} Joueur;
+
+
+
+void init_joueurs(Joueur joueurs[]);
+
+
 #endif //TP_LISTE1_LISTE_H
