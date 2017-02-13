0188a894079f160f02691eae259a404a6045d2a7
kev43 <kev43200@gmail.com>
2017-02-13 15:14:30 +0100

    Debut creation main

--- main.c
+++ main.c
@@ -4,9 +4,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "liste.h"
+
 
 int main(void)
 {
     //Création d'un tableau qui va contenir la liste
+    Joueur joueurs[NB_JOUEURS] ;
+    int tete = 0 ;
+    int nbTours = 0 ;
+
+    //on défini le nombre de tours
+    srand(time(NULL));
+    nbTours = (int)rand()/RAND_MAX ;
+
+    init_joueurs(joueurs) ;
+
     return  0  ;
 }
